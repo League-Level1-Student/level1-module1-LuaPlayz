1a79fa7074e72b85418c3f3446d50794239f88a6
chloemcbook <chloemcbook@chloes-mbp.lan>
2020-12-02 18:27:24 -0800

    yes

---
+++ src/_02_cat/CatRunner.java
@@ -0,0 +1,29 @@
+package _02_cat;
+
+public class CatRunner {
+	
+public static void main(String[] args) {
+	Cat m= new Cat("Cat");
+m.meow();
+m.printName();
+for (int i = 0; i < 9; i++) {
+m.kill();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ src/_04_popcorn/PopcornMaker.java
@@ -0,0 +1,21 @@
+package _04_popcorn;
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	Microwave m= new Microwave();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+
+}
